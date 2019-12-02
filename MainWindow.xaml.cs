ddf57cdb0b97b170babf968c4d8ef8bcf3e07328
Simon Chaveneau <simon.chaveneau@ariadnext.com>
2019-12-02 14:47:16 +0100

    Sample app displaying a webcam feed using EMGU

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Camera_EMGU_WPFSample
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        VideoCapture _capture = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartCamera();
+        }
+        public void StartCamera()
+        {
+            _capture = new VideoCapture(0);
+            _capture.ImageGrabbed += Capture_ImageGrabbed;
+            _capture.Start();
+        }
+
+        private void Capture_ImageGrabbed(object sender, EventArgs e)
+        {
+            Mat m = new Mat();
+            _capture.Retrieve(m);
+            CapturedImageBox.Invoke(new Action (() => CapturedImageBox.Image = m.ToImage<Bgr, Byte>()));
+        }
+    }
+}
